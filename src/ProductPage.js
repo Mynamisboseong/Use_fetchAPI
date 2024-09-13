7117cf4b122821ceae3e7997469d7b38ff821b0f
Mynamisboseong <wow456869@gmail.com>
2024-09-13 14:52:06 +0900

    ProductPage생성

---
+++ src/ProductPage.js
@@ -0,0 +1,18 @@
+/* state 구조
+*   {
+*       productId: 1,
+*       product: product,
+*       optionData: []    
+*   }
+*/
+
+export default function ProductPage({
+    $target,
+    initialState
+}) {
+    const $product = document.createElement('div')
+
+    $target.appendChild($product)
+
+    this.setState
+}
