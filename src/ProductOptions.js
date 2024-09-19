778604001e9fa735b946f58dffff959c94970314
Mynamisboseong <wow456869@gmail.com>
2024-09-19 14:43:17 +0900

    주석공부법

--- src/ProductOptions.js
+++ src/ProductOptions.js
@@ -1,19 +1,31 @@
-export default function ProductOptions({ $target, initialState, onSelect }) {
-    const $select = document.createElement('select')
+//해당 코드의 핵심은 옵션목록을 state로 관리하고, 렌더링과 이벤트처리를 통해 사용자와 상호작용하는 기능임.
 
+//$target:ProductOptions 컴포넌트가 렌더링될 DOM요소를 의미함. select 태그를 어느 DOM요소에 추가할지 결정함.
+//initialState:초기상태로 전달되는 값. 옵션의 리스트(이름,가격,재고)
+//onSelect:사용자가 옵션을 선택했을 때 호출되는 콜백함수임. select에서 옵션을 선택하면 해당 옵션의 정보를 전달함
+export default function ProductOptions({ $target, initialState, onSelect }) {
+    
+    //select태그를 생성하고 $select DOM요소에 저장, $target에 자식요소로 추가.
+    //해당 태그는 사용자가 옵션을 선택할 수 있게해주는 UI요소임.
+    const $select = document.createElement('select')
     $target.appendChild($select)
 
+    //전달받은 initialState를 state로 저장함. state는 옵션목록을 랜더링할 때 사용됨.
     this.state = initialState
 
+    //state를 업데이트하는 역할임. nextState를 받고 state를 변경. 변경 후 render()
     this.setState = (nextState) => {
         this.state = nextState
         this.render()
     }
 
+    //옵션의 전체이름을 만드는 함수, API의 속성을 가지고 이름을 설정함
     const createOptionFullName = ({ optionName, optionPrice, stock}) => {
         return `${optionName} ${optionPrice > 0 ? `(옵션가 ${optionPrice}` : ''} | ${stock > 0 ? `재고 ${stock}` : '재고없음'}}`
     }
 
+    //select태그의 change 이벤트리스너. 선택된 옵션의 value값을 가져오고 해당 optionId와 일치하는 옵션을 this.state에서 찾음
+    //그 후 onSelect 콜백을 호출해 선택된 옵션을 전달함
     $select.addEventListener('change', (e) => {
         const optionId = parseInt(e.target.value)
 
@@ -24,6 +36,7 @@ export default function ProductOptions({ $target, initialState, onSelect }) {
         }
     })
 
+    //렌더링 함수. select태그의 옵션목록을 렌더링 함.
     this.render = () => {
         if (this.state && Array.isArray(this.state)) {
             $select.innerHTML = `
@@ -32,5 +45,6 @@ export default function ProductOptions({ $target, initialState, onSelect }) {
             `
         }
     }
+    //컴포넌트가 생성될 때 초기 state를 기반으로 한번 렌더링 함.
     this.render()
 }
--- src/ProductPage.js
+++ src/ProductPage.js
@@ -6,13 +6,35 @@
 *   }
 */
 
+//ProductPage라는 함수를 default(하나의 파일에 개체 하나만 선언되어있는
+//모듈을 내보낼 때 많이 사용함)하게 export함
 export default function ProductPage({
+    //$target:ProductPage 컴포넌트에서 만들어지는 HTML요소를 어디에 붙힐지 지정하는 역할을 함.
+    //initialState:컴포넌트의 초기상태를 정의하는데 사용됨. (제품의 정보를 initialState로 받아서 화면에 표시할 수 있음)
     $target,
     initialState
 }) {
+    //$product 라는 DOM요소에 HTML요소를 추가하고 있음
     const $product = document.createElement('div')
-
+    //HTML요소가 추가된 $product DOM요소를 다른곳에서 사용할 수 있게 $target에 자식요소로 추가함
     $target.appendChild($product)
 
-    this.setState
+    //new키워드를 통해 새로운 ProductOptions객체를 생성, 생성자 함수에 객체를 전달하여 해당객체의 초기설정
+    const productOptions = new ProductOptions({
+        //ProductOptions 컴포넌트가 랜더링 될 DOM요소를 의미. 즉 ProductOptions에서 생성된 HTML요소들이 $product안에 추가됨
+        $target: $product,
+        //ProductOptions의 초기상태로 전달됨. 상품 옵션이나 선택가능한 항목이 들어갈 수 있음. 이 데이터를 기반으로 렌더링하는데 사용됨
+        initialState: [],
+        //콜백함수임. ProductOptions 컴포넌트에서 어떤 옵션을 클릭했을 때 호출됨. 단순히 console.log(option)으로 클릭된 옵션을 출력만 하고있음.
+        onClick: (option) => {
+            console.log(option)
+        }
+    })
+
+    this.setState = nextState => {
+        console.log(nextState)
+    }
+
+    this.render = () => { }
+    this.render()
 }
