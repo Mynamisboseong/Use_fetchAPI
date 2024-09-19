99c7ce99b7ff6324de96c2e68dea492f183365f8
Mynamisboseong <wow456869@gmail.com>
2024-09-19 16:00:25 +0900

    주석추가

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -35,6 +35,50 @@ export default function ProductPage({
         console.log(nextState)
     }
 
-    this.render = () => { }
+    this.render = () => {
+            //옵션데이터를 가져오는 함수 fetchOptionData
+            const fetchOptionData = (productId) => {
+                //첫 번째 API 요청(특정 productId에 해당하는 제품 데이터를 가져옴)
+                return request(`/products/${productId}`)
+                    //두 번째 API 요청(첫 번째 요청에서 받은 제품ID에 해당하는 제품 옵션들을 가져옴)
+                    .then(product => {
+                    return request (`/product-options?product.id=${product.id}`)
+                    })
+                    //세 번째 API 요청(각 옵션 ID에 해당하는 재고 데이터를 가져옴)
+                    .then(productOptions => {
+                        //여러 옵션의 재고 정보를 병렬적으로 가져오기 위해 promise.all을 사용함
+                        return Promise.all([
+                            Promise.resolve(productOptions),
+                            Promise.all(
+                                productOptions.map(productOption => productOption.id).map(id => {
+                                    return request(`/product-option-stocks?productOption.id=${id}`)
+                                })
+                            )
+                        ])
+                    })
+                    //세 번째 요청에서 받은 재고 정보를 각 옵션에 맞게 매핑하여 optionData 배열을 만든다.
+                    //해당 배열은 각각의 옵션 정보(ID,이름,가격,재고)를 담고있음
+                    .then(data => {
+                        const [productOptions, stocks] = data
+                        const optionData = productOptions.map((productOption,i) => {
+                            const stock = stocks[i][0].stock
+
+                            return {
+                                optionId: productOption.id,
+                                optionName: productOption.optionName,
+                                optionPrice: productOption.optionPrice,
+                                stock: stock.stock
+                            }
+                        })
+                        //컴포넌트 상태 업데이트. 가져온 옵션데이터를 ProductOptions 컴포넌트에 전달, 해당 옵션들이 화면에 렌더링 될 수 있도록 setState를 호출
+                        productOptionsComponent.setState(optionData)
+                    })
+            }
+        }
+        //이 함수를 기본으로 불러지게 하기 위함
+        this.setState({
+            ...this.state,
+            optionData
+        })
     this.render()
 }
