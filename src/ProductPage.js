b995acfb658fba1a7e75b07936c1bbb6dbea00ca
Mynamisboseong <wow456869@gmail.com>
2024-09-20 14:10:22 +0900

    give up gg

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,3 +1,6 @@
+import ProductOptions from "./ProductOptions.js"
+import { request } from "./api.js"
+
 /* state 구조
 *   {
 *       productId: 1,
@@ -19,6 +22,7 @@ export default function ProductPage({
     //HTML요소가 추가된 $product DOM요소를 다른곳에서 사용할 수 있게 $target에 자식요소로 추가함
     $target.appendChild($product)
 
+    this.state = initialState
     //new키워드를 통해 새로운 ProductOptions객체를 생성, 생성자 함수에 객체를 전달하여 해당객체의 초기설정
     const productOptions = new ProductOptions({
         //ProductOptions 컴포넌트가 랜더링 될 DOM요소를 의미. 즉 ProductOptions에서 생성된 HTML요소들이 $product안에 추가됨
@@ -35,50 +39,65 @@ export default function ProductPage({
         console.log(nextState)
     }
 
-    this.render = () => {
-            //옵션데이터를 가져오는 함수 fetchOptionData
-            const fetchOptionData = (productId) => {
-                //첫 번째 API 요청(특정 productId에 해당하는 제품 데이터를 가져옴)
-                return request(`/products/${productId}`)
-                    //두 번째 API 요청(첫 번째 요청에서 받은 제품ID에 해당하는 제품 옵션들을 가져옴)
-                    .then(product => {
-                    return request (`/product-options?product.id=${product.id}`)
-                    })
-                    //세 번째 API 요청(각 옵션 ID에 해당하는 재고 데이터를 가져옴)
-                    .then(productOptions => {
-                        //여러 옵션의 재고 정보를 병렬적으로 가져오기 위해 promise.all을 사용함
-                        return Promise.all([
-                            Promise.resolve(productOptions),
-                            Promise.all(
-                                productOptions.map(productOption => productOption.id).map(id => {
-                                    return request(`/product-option-stocks?productOption.id=${id}`)
-                                })
-                            )
-                        ])
-                    })
-                    //세 번째 요청에서 받은 재고 정보를 각 옵션에 맞게 매핑하여 optionData 배열을 만든다.
-                    //해당 배열은 각각의 옵션 정보(ID,이름,가격,재고)를 담고있음
-                    .then(data => {
-                        const [productOptions, stocks] = data
-                        const optionData = productOptions.map((productOption,i) => {
-                            const stock = stocks[i][0].stock
-
-                            return {
-                                optionId: productOption.id,
-                                optionName: productOption.optionName,
-                                optionPrice: productOption.optionPrice,
-                                stock: stock.stock
-                            }
-                        })
-                        //컴포넌트 상태 업데이트. 가져온 옵션데이터를 ProductOptions 컴포넌트에 전달, 해당 옵션들이 화면에 렌더링 될 수 있도록 setState를 호출
-                        productOptionsComponent.setState(optionData)
-                    })
-            }
-        }
-        //이 함수를 기본으로 불러지게 하기 위함
-        this.setState({
-            ...this.state,
-            optionData
-        })
+    this.render = () => { }
     this.render()
+
+    // //#app이라는 id를 가진 DOM요소를 선택한다. 해당 요소는 ProductOptions컴포넌트가 렌더링 될 대상이 됨
+    // const $target = document.querySelector("#app")
+
+    // //기본으로 사용할 제품의 ID임. 제품 ID가 1인 상품의 옵션데이터를 불러오는데 사용된다.
+    // const DEFAULT_PRODUCT_ID = 1
+
+    // //옵션데이터를 불러오는 함수를 호출. 초기 로딩 시 기본적으로 1번 제품의 옵션들이 화면에 표시됨
+    // fetchOptionData(DEFAULT_PRODUCT_ID)
+
+    //옵션데이터를 가져오는 함수 fetchOptionData
+    const fetchOptionData = (productId) => {
+        //첫 번째 API 요청(특정 productId에 해당하는 제품 데이터를 가져옴)
+        return request(`/products/${productId}`)
+            //두 번째 API 요청(첫 번째 요청에서 받은 제품ID에 해당하는 제품 옵션들을 가져옴)
+            .then(product => {
+                this.setState({
+                    // ...는 전개연산자임. 배열이나 객체를 쉽게 복사, 여러 값들을 함수의 인수로 전달할 수 있음
+                    ...this.state,
+                    product,
+                    optionData: []
+                })
+            return request (`/product-options?product.id=${product.id}`)
+            })
+            //세 번째 API 요청(각 옵션 ID에 해당하는 재고 데이터를 가져옴)
+            .then(productOptions => {
+                //여러 옵션의 재고 정보를 병렬적으로 가져오기 위해 promise.all을 사용함
+                return Promise.all([
+                    Promise.resolve(productOptions),
+                    Promise.all(
+                        productOptions.map(productOption => productOption.id).map(id => {
+                            return request(`/product-option-stocks?productOption.id=${id}`)
+                        })
+                    )
+                ])
+            })
+            //세 번째 요청에서 받은 재고 정보를 각 옵션에 맞게 매핑하여 optionData 배열을 만든다.
+            //해당 배열은 각각의 옵션 정보(ID,이름,가격,재고)를 담고있음
+            .then(data => {
+                const [productOptions, stocks] = data
+                const optionData = productOptions.map((productOption,i) => {
+                    const stock = stocks[i][0].stock
+
+                    return {
+                        optionId: productOption.id,
+                        optionName: productOption.optionName,
+                        optionPrice: productOption.optionPrice,
+                        stock
+                    }
+                })
+                this.setState({
+                    ...this.state,
+                    optionData
+                })
+                //컴포넌트 상태 업데이트. 가져온 옵션데이터를 ProductOptions 컴포넌트에 전달, 해당 옵션들이 화면에 렌더링 될 수 있도록 setState를 호출
+                productOptionsComponent.setState(optionData)
+            })
+    }
+    fetchOptionData(this.setState.productId)
 }
--- src/main.js
+++ src/main.js
@@ -1,59 +1,17 @@
-//외부모듈 import.
-import ProductOptions from "./ProductOptions.js"
-import { request } from "./api.js"
+import ProductPage from './ProductPage.js'
 
-//#app이라는 id를 가진 DOM요소를 선택한다. 해당 요소는 ProductOptions컴포넌트가 렌더링 될 대상이 됨
-const $target = document.querySelector("#app")
+const $target = document.querySelector('#app')
 
-//기본으로 사용할 제품의 ID임. 제품 ID가 1인 상품의 옵션데이터를 불러오는데 사용된다.
-const DEFAULT_PRODUCT_ID = 1
-
-//옵션데이터를 가져오는 함수 fetchOptionData
-const fetchOptionData = (productId) => {
-    //첫 번째 API 요청(특정 productId에 해당하는 제품 데이터를 가져옴)
-    return request(`/products/${productId}`)
-        //두 번째 API 요청(첫 번째 요청에서 받은 제품ID에 해당하는 제품 옵션들을 가져옴)
-        .then(product => {
-        return request (`/product-options?product.id=${product.id}`)
-        })
-        //세 번째 API 요청(각 옵션 ID에 해당하는 재고 데이터를 가져옴)
-        .then(productOptions => {
-            //여러 옵션의 재고 정보를 병렬적으로 가져오기 위해 promise.all을 사용함
-            return Promise.all([
-                Promise.resolve(productOptions),
-                Promise.all(
-                    productOptions.map(productOption => productOption.id).map(id => {
-                        return request(`/product-option-stocks?productOption.id=${id}`)
-                    })
-                )
-            ])
-        })
-        //세 번째 요청에서 받은 재고 정보를 각 옵션에 맞게 매핑하여 optionData 배열을 만든다.
-        //해당 배열은 각각의 옵션 정보(ID,이름,가격,재고)를 담고있음
-        .then(data => {
-            const [productOptions, stocks] = data
-            const optionData = productOptions.map((productOption,i) => {
-                const stock = stocks[i][0].stock
-
-                return {
-                    optionId: productOption.id,
-                    optionName: productOption.optionName,
-                    optionPrice: productOption.optionPrice,
-                    stock: stock.stock
-                }
-            })
-            //컴포넌트 상태 업데이트. 가져온 옵션데이터를 ProductOptions 컴포넌트에 전달, 해당 옵션들이 화면에 렌더링 될 수 있도록 setState를 호출
-            productOptionsComponent.setState(optionData)
-        })
-}
-
-//옵션데이터를 불러오는 함수를 호출. 초기 로딩 시 기본적으로 1번 제품의 옵션들이 화면에 표시됨
-fetchOptionData(DEFAULT_PRODUCT_ID)
-//ProductOptions 컴포넌트 초기화
-const productOptionsComponent = new ProductOptions({
+new ProductPage({
     $target,
-    initialState: [],
-    onSelect: (option) => {
-        alert(option.optionName)
+    initialState: {
+        productId: 1,
+
     }
-})
+})  
+
+
+
+
+
+
